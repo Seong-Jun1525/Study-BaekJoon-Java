67ac114c131e91c556fe9b88e7f574934f109e8b
Seong-Jun1525 <sjsj123455@naver.com>
2025-02-19 00:45:36 +0900

    9935 풀이- 스택

--- src/gold/stack/Study9935.java
+++ src/gold/stack/Study9935.java
@@ -22,21 +22,67 @@ public class Study9935 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		Stack<Character> stack = new Stack<>();
+		String str = br.readLine();      // 원본 문자열
+        String bombStr = br.readLine();  // 폭발 문자열
+        int bombLen = bombStr.length();  // 폭발 문자열 길이
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            stack.push(ch);
+
+            // 스택의 끝 부분이 폭발 문자열과 일치하는지 확인
+            if (stack.size() >= bombLen) {
+                boolean isBomb = true;
+
+                // 폭발 문자열이 일치하는지 확인
+                for (int i = 0; i < bombLen; i++) {
+                    if (stack.get(stack.size() - bombLen + i) != bombStr.charAt(i)) {
+                        isBomb = false;
+                        break;
+                    }
+                }
+
+                // 일치하면 폭발 문자열 길이만큼 pop
+                if (isBomb) {
+                    for (int i = 0; i < bombLen; i++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+
+        // 스택이 비어 있으면 "FRULA" 출력
+        if (stack.isEmpty()) {
+            bw.write("FRULA\n");
+        } else {
+            StringBuilder result = new StringBuilder();
+            for (char c : stack) {
+                result.append(c);
+            }
+            bw.write(result.toString() + "\n");
+        }
+		// 메모리 초과
+//		boolean flag = false;
+//		
+//		String str = br.readLine();
+//		String bombStr = br.readLine();
+//		if(str.contains(bombStr)) {
+//			while(str.contains(bombStr)) {
+//				str = str.replaceAll(bombStr, "");
+//			}
+//			if(str.length() > 0) flag = true;
+//		} 
+//
+//		if(flag || !str.isEmpty()) {
+//			bw.write(str + "\n");
+//		} else {
+//			bw.write("FRULA" + "\n");
+//		}
 		
-		String str = br.readLine();
-		String bombStr = br.readLine();
-		int j = 0;
-		while(true) {
-			if(str.contains(bombStr)) {
-				for(int i = 0; i < str.length(); i++) {
-					char c = str.charAt(i);
-					
-				}
-			} else {
-				break;
-			}
-		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
